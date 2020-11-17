3cd27aec4dc147e772a2394ce3068cf341ac1bf9
John Paul Antonovich <antonovichJP@gmail.com>
2020-11-17 10:57:07 -0800

    testing

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Author: John Paul Antonovich
+function getSymbols() {
+	let symbolFile = new XMLHttpRequest();
+	symbolFile.open("GET", "symbols.txt", true);
+	symbolFile.onreadystatechange = function() {
+		if (symbolFile.readyState === 4) {
+			if (symbolFile.status === 200 || symbolFile.status == 0) {
+				let symbolText = symbolFile.responseText;
+				alert(symbolText);
+			}			
+		}
+	}
+	symbolFile.send(null);
+}
+
